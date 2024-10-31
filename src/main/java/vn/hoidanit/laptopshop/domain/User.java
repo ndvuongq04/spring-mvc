2a6141eaa8f391fb162550eefbb0572522ecc27f
ndvuongq04 <nguyendinhvuong08122004@gmail.com>
2024-10-31 17:01:05 +0700

    add video 77

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -5,9 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 // cho spring biet se tao 1 table trong database , co ten nhu class 
 @Entity
+@Table(name="users")
 public class User {
     @Id // cho biet day se la ID
     @GeneratedValue(strategy = GenerationType.IDENTITY) // cho spring biet tu dong danh ID
