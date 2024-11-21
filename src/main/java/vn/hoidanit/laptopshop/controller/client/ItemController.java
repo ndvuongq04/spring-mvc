ccc8090f1d88631f24fcca9c05f27a063f017b42
ndvuongq04 <nguyendinhvuong08122004@gmail.com>
2024-11-21 17:39:06 +0700

    add video 106

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -5,11 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.service.ProductService;
+
 @Controller
 public class ItemController {
+    // DI
+    private final ProductService productService;
+
+    public ItemController(ProductService productService) {
+        this.productService = productService;
+    }
 
     @GetMapping("/product/{id}")
     public String getProductPage(Model model, @PathVariable long id) {
+        Product prOfId = this.productService.fetchProductById(id).get(); // .get() để lấy ra đối tượng product
+        model.addAttribute("prOfId", prOfId);
         return "client/product/detail";
     }
 }
